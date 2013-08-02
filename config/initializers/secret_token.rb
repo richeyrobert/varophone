e003f299793be03be0c52f2520d3b43fb298cde6
Robert Richey <rob@rocketnext.com>
2013-08-01 21:07:50 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Varophone::Application.config.secret_token = '8c1c4bba9c5599d1ab97a78c282e1d26aeb7691ad18edc32071b176a7d0988bf7a5ee338dd264d1069c755a201423722d9583b9bf7e9e5788fdf691328aaabea'
